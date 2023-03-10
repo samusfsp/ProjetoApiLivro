77573932fd1a3d1be34c01a24eb5b7b4497f52df
“Flavio <samus.sp@gmail.com>
2023-03-10 16:06:45 -0300

    adição de endpoints de boas vindas

---
+++ src/main/java/br/com/flaviodev/livros/controller/BoasVindasConreoller.java
@@ -0,0 +1,22 @@
+package br.com.flaviodev.livros.controller;
+
+import org.hibernate.property.access.internal.PropertyAccessStrategyNoopImpl;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BoasVindasConreoller {
+	
+	@GetMapping
+	public String BoasVindas() {
+		return "<pre>"
+				
+				+ "Bem vindo a minha API de livros<br/>"
+				+ "Endpoints Disponiveis<br/>"
+				+ "/livro get<br/>"
+				+ "/livros/id por id<br/>"
+				+"/livros post adicionar<br/>"
+	}
+	
+	
+}
